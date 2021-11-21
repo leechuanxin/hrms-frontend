ccbf05da3b6c9074b8c21178854de91147247754
Lee Chuan Xin <chuanxin@gametize.com>
2021-11-21 18:28:04 +0800

    fix: show only next month in admin index.

--- src/components/Index/IndexPage.jsx
+++ src/components/Index/IndexPage.jsx
@@ -9,9 +9,22 @@ import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 export default function IndexPage() {
   const newUserId = 51;
   const [events, setEvents] = useState([
-    { title: '', date: '2021-11-03', extendedProps: { user_id: newUserId, real_name: 'Lee Chuan Xin', type: 'shift' } },
-    { title: '', date: '2021-11-07', extendedProps: { user_id: newUserId, real_name: 'Lee Chuan Xin', type: 'leave' } },
+    { title: '', date: '2021-12-03', extendedProps: { user_id: newUserId, real_name: 'Lee Chuan Xin', type: 'shift' } },
+    { title: '', date: '2021-12-07', extendedProps: { user_id: newUserId, real_name: 'Lee Chuan Xin', type: 'leave' } },
   ]);
+
+  const getNextMonthString = (date) => {
+    const formatter = new Intl.DateTimeFormat('default', { month: 'long' });
+    const nextMonthDateStr = formatter.format(date);
+    return nextMonthDateStr;
+  };
+
+  const getNextMonthDate = (date) => {
+    const nextMonthNumber = date.getMonth() + 1;
+    const nextMonthDate = new Date(date.getFullYear(), nextMonthNumber, 1);
+    return nextMonthDate;
+  };
+  const [nextMonthDate] = useState(getNextMonthDate(new Date()));
 
   useEffect(() => {
     const newEvents = [...events];
@@ -36,8 +49,19 @@ export default function IndexPage() {
   return (
     <div className="container-fluid pt-5">
       <div className="row w-100 pt-3">
-        <div className="col-12 pt-1 d-flex justify-content-end">
-          <Link className="btn btn-primary" to="/edit" role="button">Edit</Link>
+        <div className="col-12 pt-1">
+          <div className="row align-items-center">
+            <div className="col-6">
+              <h3>
+                Shift Overview →
+                {' '}
+                {getNextMonthString(nextMonthDate)}
+              </h3>
+            </div>
+            <div className="col-6  d-flex justify-content-end">
+              <Link className="btn btn-primary" to="/adminedit" role="button">Edit</Link>
+            </div>
+          </div>
         </div>
         <div className="col-12"><hr /></div>
         <div className="col-12 pt-1">
@@ -45,6 +69,8 @@ export default function IndexPage() {
             plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
             events={events}
+            initialDate={nextMonthDate}
+            headerToolbar={false}
           />
         </div>
         <div className="col-12 pt-3" />
